63334d6843357d5263b5d95a32fb0e5e8dc9c297
Your Name <you@example.com>
2021-04-01 11:27:27 -0500

    3 commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/clientes',c_clientes);
 app.get('/productos',c_productos);
 
 function c_inicio(req,res){
-    res.send('<h1>Express Inicio</h1>')
+    res.send('<h1>Express Inicio - Ruth Carbajal</h1>')
 }
 
 function c_clientes(req,res){
